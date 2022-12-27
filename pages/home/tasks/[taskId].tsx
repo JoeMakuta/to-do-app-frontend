ae9869201245256b2dfc8bedce51458f040692fb
Josué Makuta <makutajosue@gmail.com>
2022-12-27 08:54:05 +0100

    Deploy

--- pages/home/tasks/[taskId].tsx
+++ pages/home/tasks/[taskId].tsx
@@ -7,8 +7,9 @@ const SingleTask = (): JSX.Element => {
 
 export default SingleTask;
 
-export const getStaticPaths = async (context: any): any => {
+export const getStaticPaths = async (context: any): Promise<any> => {
   const API = process.env.NEXT_PUBLIC_BACKEND_URI;
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/restrict-template-expressions
   const res = await axios.get(`${API}/users`);
 };
